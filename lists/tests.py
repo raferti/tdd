137c6113600392cd920bee263c5276380d0251cd
raferti <raferti2@yandex.ru>
2020-12-23 21:08:05 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
+    """Тест домашней страницы"""
 
-    def test_bad_master(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page(self):
+        """Тест корневой url преобразуется в представления"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
+
