bde9a19c7bab84d58c9d7ac445f605fd90aad78d
raferti <raferti2@yandex.ru>
2020-12-23 22:39:17 +0300

    Рефакторинг представления дмашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -7,18 +7,11 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        """Тест домашняя страница возвращает корректный html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+
 
 
 
