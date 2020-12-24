cda356f6fc17ce148912549d4c58dab4b092b848
raferti <raferti2@yandex.ru>
2020-12-24 16:33:39 +0300

    Создали новый url, view и шаблон

--- django_todo_tdd/urls.py
+++ django_todo_tdd/urls.py
@@ -4,5 +4,6 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lists/first-list/', views.view_list, name='view_list'),
     path('admin/', admin.site.urls),
 ]
--- lists/tests.py
+++ lists/tests.py
@@ -23,21 +23,11 @@ class HomePageTest(TestCase):
         """Переадресация после POST запроса"""
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/first-list/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_display_all_list_items(self):
-        """Отображаются все элементы списка"""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -60,6 +50,24 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+    """Тест представления списка"""
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/first-list/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/first-list/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 
 
 
