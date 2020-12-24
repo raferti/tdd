15811eb85b46706c980d997abd69f3b2553aedc7
raferti <raferti2@yandex.ru>
2020-12-24 12:27:16 +0300

    Модель для элементов списка

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -16,6 +17,30 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    """Тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
 
 
 
