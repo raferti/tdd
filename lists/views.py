aca18f6f67514baa7db262319c82868c6354d033
raferti <raferti2@yandex.ru>
2020-12-24 11:11:30 +0300

    Добавил тесты

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
     """Домашняя страница"""
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''), })
