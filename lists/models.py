b84f209e65ece1bb97f6daf021cee1a6fbd445e9
raferti <raferti2@yandex.ru>
2020-12-24 17:30:07 +0300

    Добавили новую модель списка и изменили тесты под неё

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class List(models.Model):
+    """Список"""
+    pass
+
+
 class Item(models.Model):
     """Элемент списка"""
     text = models.TextField(default='', null=True)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -17,6 +17,7 @@ def view_list(request):
 
 def new_list(request):
     """Новый список"""
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/first-list/')
 
